b41d542062cdba8d46a0e939e340f683b2eebf0d
Crowdbotics <team@crowdbotics.com>
2020-10-30 08:40:21 +0000

    Initial commit.

---
+++ backend/nilai_sidang_22115/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nilai_sidang_22115 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nilai_sidang_22115.settings')
+
+application = get_wsgi_application()
